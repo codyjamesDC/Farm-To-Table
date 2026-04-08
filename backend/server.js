706781c47cef50fd7c628a0dbee90ac8b740823f
AzeOcram <mrodon@up.edu.ph>
2026-04-08 21:51:28 +0800

    chore: updated server.js using .env file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 //Express Server
-
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// this tells our server to listen to the port 3000
-app.listen(3000, () => { console.log('Server started at port 3000')} );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
