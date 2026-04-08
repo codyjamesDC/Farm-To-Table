7d2d3a5820afb5a4b038413c73362a1c30933e8b
AzeOcram <mrodon@up.edu.ph>
2026-04-08 22:45:15 +0800

    feat: setup db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 //Express Server
 import express from 'express';
+import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
+
+connectDB(); //connect to db
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
