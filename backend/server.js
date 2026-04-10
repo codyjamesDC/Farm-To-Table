ba8a0809ea42f972e4c531160492d2ff2bfa50e5
duane01-uplb <dsmendoza5@up.edu.ph>
2026-04-10 18:27:11 +0800

    chore: cleans server.js

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,20 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import routes from './routes/router.js';
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 3000;
+connectDB();
+routes(app);
 
-
-connectDB(); //connect to db
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
