b1e85c519b92cce3015abd57d7005a4b2e7873aa
AzeOcram <mrodon@up.edu.ph>
2026-04-22 00:46:29 +0800

    feat: new User Routes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,6 +2,7 @@ import productRoutes from './productRoutes.js';
 import authRoutes from './authRoutes.js';
 import cartRoute from './cartRoutes.js';
 import adminRoutes from './adminRoutes.js'
+import userRoutes from './userRoutes.js'
 
 
 export default (app) => {
@@ -17,4 +18,5 @@ export default (app) => {
     app.use('/api/auth', authRoutes);
     app.use('/api/cart', cartRoute);
     app.use('/api/admin', adminRoutes);
+    app.use('/api/profile', userRoutes)
 }
