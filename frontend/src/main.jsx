4bcd0583c46ed0c0cd9f1a652f08f9bfedc1b375
Ralphyy <rbcrisostomo09@gmail.com>
2026-04-21 16:43:11 +0800

    chore: adds routing provider for homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Children } from 'react';
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx"; 
+
+// import main app components from pages
+const router = createBrowserRouter([
+  { path: "/", element: <App />, children: [
+    { path: "home", element: <Home /> },
+    { path: "login", element: <Login /> }
+  ]
+  }
+]);
+
+// render everything using router provider
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
