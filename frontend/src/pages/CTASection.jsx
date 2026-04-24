b6be1612df0b4695d5602e9cee59ece81bfce38e
codyjamesDC <cbdelacruz10@up.edu.ph>
2026-04-24 18:21:53 +0800

    feat: add the skeleton of Call To Action Section in the homepage

---
+++ frontend/src/pages/CTASection.jsx
@@ -0,0 +1,23 @@
+export default function CTASection() {
+    return (
+        <section>
+
+        {/* Background Image + Overlay */}
+        <div>
+            <img
+            src="https://images.unsplash.com/photo-1744659750204-87034350eec6?w=1920&q=80"
+            alt="Fresh produce"
+            />
+            <div></div>
+        </div>
+
+        {/* Content */}
+        <div>
+            <h2>Support Local Farmers Today</h2>
+            <p>Every purchase makes a difference in our farming communities</p>
+            <a href="#harvest">Start Shopping</a>
+        </div>
+
+        </section>
+    );
+}
